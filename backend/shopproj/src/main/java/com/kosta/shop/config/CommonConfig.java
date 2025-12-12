511b89f04f1db91fb964b1b1b85bf3c89df85a10
gh3298 <gh3298@gmail.com>
2025-12-12 17:44:12 +0900

    QUERYDSL 플러그인방식에서 의존성으로 변경

--- backend/shopproj/src/main/java/com/kosta/shop/config/CommonConfig.java
+++ backend/shopproj/src/main/java/com/kosta/shop/config/CommonConfig.java
@@ -1,14 +1,27 @@
 package com.kosta.shop.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 @Configuration
 public class CommonConfig {
+	
+	@PersistenceContext
+    private EntityManager entityManager;
 
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+	
+	@Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
