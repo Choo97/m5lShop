446b5c609dee10ba3934a33958032ffdcc33b924
gh3298 <gh3298@gmail.com>
2025-12-23 01:31:59 +0900

    백엔드 배포포트 접속허용

--- backend/shopproj/src/main/java/com/kosta/shop/config/CorsConfig.java
+++ backend/shopproj/src/main/java/com/kosta/shop/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
 	    CorsConfiguration config = new CorsConfiguration();
 	    config.setAllowCredentials(true);  // 쿠키/Authorization 허용
 	    config.addAllowedOriginPattern("http://localhost:5173"); // React dev 서버
+	    config.addAllowedOriginPattern("http://16.184.56.172:5173"); // React prod 서버
 	    config.addAllowedHeader("*"); // 모든 헤더 허용
 	    config.addAllowedMethod("*"); // 모든 메서드 허용
 	    config.addExposedHeader(JwtProperties.HEADER_STRING); //react에서 토큰을 헤더로 받기 위해 받드시 필요
